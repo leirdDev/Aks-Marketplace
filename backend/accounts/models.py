9051826dab1504942a667a4eecf1052ddea75afa
Adriel <Tempest20a@gmail.com>
2026-01-06 22:51:11 +0800

    feat(accounts): require first_name and last_name and register CustomUser in admin

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -2,4 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
+  first_name = models.CharField(max_length=150)
+  last_name = models.CharField(max_length=150)
   profile_url = models.URLField(blank=True)
+
+  REQUIRED_FIELDS = ["first_name", "last_name"]
